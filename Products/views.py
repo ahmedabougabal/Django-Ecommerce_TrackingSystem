42dc51fb58f2d08d0d94fe9bed2c60520c624081
ahmedabougabal <ahmedabougabal@live.com>
2024-09-09 00:45:48 +0300

    feat: Add product serialization with REST API endpoint in Products app
    - Added ProductSerializer in Products/serializers.py to serialize Product model fields (id, name, price, active, image).
    - Defined product_list view in Products/views.py to handle GET requests for active products.
      - Integrated ProductSerializer in the product_list view to serialize and return active products as JSON.
    - Updated Products/urls.py to include the /products_list/ endpoint, pointing to the product_list view.
    - Ensured that the Products app URL is properly included in the main ecommerce/urls.py file, allowing access to the /Products/products_list/ endpoint.

--- Products/views.py
+++ Products/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
-def index(request):
-  pass
-
-def Products(request):
-  pass
+@api_view(['GET'])
+def product_list(request):
+    # active products from the database and then we do serialize!!
+    active_products = Product.objects.filter(active=True)
+    serializer = ProductSerializer(active_products, many=True)
+    # Return the serialized complex data as JSON to be dealt with easily :D
+    return Response(serializer.data)
